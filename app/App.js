19421ec01adb6a11ec1577c3f507ab95bcdb5791
Matthew Sperry <mattjsperry@gmail.com>
2017-12-11 11:48:13 -0800

    Add react-native files

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
