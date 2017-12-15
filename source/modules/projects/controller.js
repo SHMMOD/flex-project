560915175a7a6b8643531bd108fa60e966df967a
Omar <omar.dlsg@gmail.com>
2017-12-15 10:16:04 -0800

    GET routes for users and projects

--- source/modules/projects/controller.js
+++ source/modules/projects/controller.js
@@ -16,15 +16,10 @@ export const postProject = async (req, res) => {
     name: req.body.name,
     userId: req.body.userId
   });
+
   console.log(`New Project: ${newProject}`);
 
   let savedNewProject;
-
-  // try {
-  //   savedNewProject = newProject.save();
-  // } catch (err) {
-  //
-  // }
 
   try {
     savedNewProject = await newProject.save()
@@ -32,31 +27,9 @@ export const postProject = async (req, res) => {
         console.log(err);
         res.status(500).json({ error: true, message: 'Error with project' })
       })
-
+      // sends new project object down if it saves successfully 
       res.status(201).json(savedNewProject);
     } catch (err) {
       res.status(err.status).json({ error: true, message: 'Error saving project' });
   }
-
-  // try {
-  //   return res.status(201).json(savedNewProject);
-  // } catch (err) {
-  //   console.log("INSIDE FIRST CATCH")
-  //   return res.status(err.status).json({ error: true, message: 'Error with project' });
-  // }
-
-  // try {
-  //     return res.status(201).json(await newProject.save());
-  //   } catch (err) {
-  //     return res.status(err.status).json({ error: true, message: 'Error with project' });
-  //   }
-
-  // const newProject = new Project({
-  //   _id: new mongoose.Types.ObjectId(),
-  //   name: "Sample 4",
-  //   userId: "5a3355134ecec93ed167650c"
-  // });
-  //
-  // const savedNewProject = await newProject.save();
-  // res.send(savedNewProject);
 };
