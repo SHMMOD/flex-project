f4d2a1bb3a9964c51f32201f3861a9fa732f70fa
Omar <omar.dlsg@gmail.com>
2017-12-15 09:56:42 -0800

    Refactor project POST

--- source/modules/projects/controller.js
+++ source/modules/projects/controller.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 import { Project } from './model';
 
 export const getProject = async (req, res) => {
@@ -6,4 +8,55 @@ export const getProject = async (req, res) => {
   } catch (err) {
     return res.status(400).json({ error: true, message: 'Cannot find project' });
   }
+};
+
+export const postProject = async (req, res) => {
+  const newProject = new Project({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    userId: req.body.userId
+  });
+  console.log(`New Project: ${newProject}`);
+
+  let savedNewProject;
+
+  // try {
+  //   savedNewProject = newProject.save();
+  // } catch (err) {
+  //
+  // }
+
+  try {
+    savedNewProject = await newProject.save()
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: true, message: 'Error with project' })
+      })
+
+      res.status(201).json(savedNewProject);
+    } catch (err) {
+      res.status(err.status).json({ error: true, message: 'Error saving project' });
+  }
+
+  // try {
+  //   return res.status(201).json(savedNewProject);
+  // } catch (err) {
+  //   console.log("INSIDE FIRST CATCH")
+  //   return res.status(err.status).json({ error: true, message: 'Error with project' });
+  // }
+
+  // try {
+  //     return res.status(201).json(await newProject.save());
+  //   } catch (err) {
+  //     return res.status(err.status).json({ error: true, message: 'Error with project' });
+  //   }
+
+  // const newProject = new Project({
+  //   _id: new mongoose.Types.ObjectId(),
+  //   name: "Sample 4",
+  //   userId: "5a3355134ecec93ed167650c"
+  // });
+  //
+  // const savedNewProject = await newProject.save();
+  // res.send(savedNewProject);
 };
--- source/modules/projects/routes.js
+++ source/modules/projects/routes.js
@@ -3,12 +3,14 @@ import * as ProjectController from './controller';
 
 const routes = new Router();
 
+routes.get('/projects/:projectId', ProjectController.getProject);
+routes.post('/projects', ProjectController.postProject);
+
 // routes.get('/users', ProjectController.getAllUsers);
 // routes.post('/users', ProjectController.createUser);
 // routes.post('/users/auth0', ProjectController.loginWithAuth0);
 // routes.get('/users/seed', ProjectController.seedEvents);
 
-routes.get('/projects/:projectId', ProjectController.getProject);
 // routes.put('/users/:userId', ProjectController.updateUser);
 
 export default routes;
