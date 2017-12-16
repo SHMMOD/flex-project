0586d1ab98349c3c74ec3ed16760a2b11aba1b18
Omar <omar.dlsg@gmail.com>
2017-12-16 11:31:43 -0800

    Can PUT idea

--- source/modules/ideas/controller.js
+++ source/modules/ideas/controller.js
@@ -35,3 +35,34 @@ export const postIdea = async (req, res) => {
       res.status(err.status).json({ error: true, message: 'Error saving idea' });
   }
 };
+
+export const editIdea = async (req, res) => {
+  let queriedIdea;
+  try {
+    queriedIdea = await Idea.findById(req.params.ideaId);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: true, message: 'Error finding project' });
+  }
+
+  console.log(`Queried Idea to Edit: ${queriedIdea}`);
+
+  // allowable fields to update
+  queriedIdea.name = req.body.name || queriedIdea.name;
+  queriedIdea.body = req.body.body || queriedIdea.body;
+  queriedIdea.url = req.body.url || queriedIdea.url;
+  queriedIdea.favorited = req.body.favorited || queriedIdea.favorited;
+
+  let savedIdea;
+  try {
+    savedIdea = await queriedIdea.save()
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: true, message: 'Error saving idea' })
+      })
+      // sends new project object down if it saves successfully
+      res.status(200).json(savedIdea);
+    } catch (err) {
+      res.status(err.status).json({ error: true, message: 'Error saving idea' });
+  }
+};
--- source/modules/ideas/routes.js
+++ source/modules/ideas/routes.js
@@ -5,5 +5,6 @@ const routes = Router();
 
 routes.get('/ideas/:ideaId', IdeaController.getIdea);
 routes.post('/ideas', IdeaController.postIdea);
+routes.put('/ideas/:ideaId', IdeaController.editIdea);
 
 export default routes;
