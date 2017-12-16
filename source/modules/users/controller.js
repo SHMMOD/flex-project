f4e517032496294d92261c99043967371052dfc4
Omar <omar.dlsg@gmail.com>
2017-12-15 16:44:05 -0800

    GET user sends down an array of associated projects

--- source/modules/users/controller.js
+++ source/modules/users/controller.js
@@ -1,9 +1,10 @@
 import { User } from './model';
+import { Project } from '../projects/model';
 
 export const getUser = async (req, res) => {
   let results = {};
-  let queriedUser;
 
+  let queriedUser;
   try {
     queriedUser = await User.findById(req.params.userId)
       .catch(err => {
@@ -14,11 +15,24 @@ export const getUser = async (req, res) => {
   } catch (err) {
     return res.status(500).json({ error: true, message: 'Cannot find user' });
   }
+  results["user"] = queriedUser;
 
-results["user"] = queriedUser;
-res.status(200).json(queriedUser);
+  let queriedProjects;
+  try {
+    queriedProjects = await Project.find({userId: queriedUser._id})
+      .catch(err => {
+        console.log(err);
+        res.status(400).json({ error: true, message: 'Could not find project' })
+      })
+    // res.status(200).json(queriedUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Cannot find project' });
+  }
+  results["projects"] = queriedProjects;
 
 
+  res.status(200).json(results);
 };
 
 // FUNCTIONAL
