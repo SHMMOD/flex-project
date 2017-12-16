1894bc36ad0616dbf97c6ba3d9ca57dcaf45162e
Omar <omar.dlsg@gmail.com>
2017-12-16 10:40:58 -0800

    Can POST idea

--- source/modules/ideas/controller.js
+++ source/modules/ideas/controller.js
@@ -9,3 +9,29 @@ export const getIdea = async (req, res) => {
     return res.status(400).json({ error: true, message: 'Cannot find idea' });
   }
 };
+
+export const postIdea = async (req, res) => {
+  const defaultName = req.body.fileType === 'note' ? 'New Note' : 'New Recording';
+
+  const newIdea = new Idea({
+    _id: new mongoose.Types.ObjectId(),
+    fileType: req.body.fileType,
+    name: defaultName,
+    projectId: req.body.projectId,
+    userId: req.body.userId
+  });
+
+  console.log(`New Idea: ${newIdea}`);
+
+  let savedNewIdea;
+  try {
+    savedNewIdea = await newIdea.save()
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: true, message: 'Error saving idea' })
+      })
+      res.status(200).json(savedNewIdea);
+    } catch (err) {
+      res.status(err.status).json({ error: true, message: 'Error saving idea' });
+  }
+};
--- source/modules/ideas/routes.js
+++ source/modules/ideas/routes.js
@@ -4,5 +4,6 @@ import * as IdeaController from './controller';
 const routes = Router();
 
 routes.get('/ideas/:ideaId', IdeaController.getIdea);
+routes.post('/ideas', IdeaController.postIdea);
 
 export default routes;
